0eadf84f16c4f6baef5af5d49cec74a21166fee9
solomon <solomon.1987.2020@gmail.com>
2023-12-06 00:08:09 +0200

    Hoome Woorks

--- HW_5_Kramarev.py
+++ HW_5_Kramarev.py
@@ -10,56 +10,30 @@
 #    print()
 
 
-
-addition_table = {}
-for i in range(2, 10):
-    addition_table[i] = {}
-    for j in range(2, 10):
-        addition_table[i][j] = i + j
-
-multiplication_table = {}
-for i in range(2, 10):
-    multiplication_table[i] = {}
-    for j in range(2, 10):
-        multiplication_table[i][j] = i * j
-
-subtraction_table = {}
-for i in range(2, 10):
-    subtraction_table[i] = {}
-    for j in range(2, 10):
-        subtraction_table[i][j] = i - j
-
-division_table = {}
-for i in range(2, 10):
-    division_table[i] = {}
-    for j in range(2, 10):
-        division_table[i][j] = i / j
-
-
 operation = input("Виберіть операцію { +, -, *, / } ")
 
 if operation == "+":
     for i in range(2, 10):
         for j in range(2, 10):
-            print(f"{i} + {j} = {addition_table[i][j]}", end=" ")
+            print(f"{i} + {j} = {i + j}", end=" ")
         print()
 
 elif operation == "-":
     for i in range(2, 10):
         for j in range(2, 10):
-            print(f"{i} - {j} = {subtraction_table[i][j]}", end=" ")
+            print(f"{i} - {j} = {i - j}", end=" ")
         print()
 
 elif operation == "*":
     for i in range(2, 10):
         for j in range(2, 10):
-            print(f"{i} * {j} = {multiplication_table[i][j]}", end=" ")
+            print(f"{i} * {j} = {i * j}", end=" ")
         print()
 
 elif operation == "/":
     for i in range(2, 10):
         for j in range(2, 10):
-            print(f"{i} / {j} = {division_table[i][j]}", end=" ")
+            print(f"{i} / {j} = {i / j}", end=" ")
         print()
 else:
     print("Немає такої операції")
