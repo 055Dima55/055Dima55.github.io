00e903fc352cdbc66afdbef3e341db849fb339ab
solomon <solomon.1987.2020@gmail.com>
2024-01-31 22:27:06 +0200

    HW 12

--- HW_12_Kramarev/conftest.py
+++ HW_12_Kramarev/conftest.py
@@ -9,3 +9,13 @@ def fixture_random(request):
     request.cls.response = response
     request.cls.status_code = status_code
     yield response, status_code
+
+
+@pytest.fixture(scope="class")
+def chuck_norris_api(request):
+    URL = "https://api.chucknorris.io/jokes/search?query=Horses"
+    response = requests.request(method="GET", url=URL)
+    request.cls.response = response
+    j_son = response.json()
+    request.cls.j_son = j_son
+    yield response, j_son
--- HW_12_Kramarev/test_HW_chucknorris.py
+++ HW_12_Kramarev/test_HW_chucknorris.py
@@ -10,58 +10,43 @@
 тест на кількість жартів
 тест на сам жарт
 + 3 тести '''
-
 import pytest
 import requests
 
 
-@pytest.mark.usefixtures("fixture_random")
+@pytest.mark.usefixtures('fixture_random')
 class TestRandom:
-    def test_icon_url_not_empty(self):
+    def test_icon_url_not_empty(self, fixture_random):
         assert self.response.json()["icon_url"] is not None
 
-    def test_icon_url_png(self):
+    def test_icon_url_png(self, fixture_random):
         assert self.response.json()["icon_url"].endswith(".png")
 
-    def test_value_not_empty(self):
+    def test_value_not_empty(self, fixture_random):
         assert self.response.json()["value"] is not None
 
-    def test_value_meaning(self):
-        assert int(self.response.json()["value"]) > 10
+    def test_value_meaning(self, fixture_random):
+        assert len(self.response.json()["value"]) > 10
 
 
-
-@pytest.SpecificWordfixture(scope="class")
-def chuck_norris_api():
-    URL = "https://api.chucknorris.io/jokes/search?query=Horses"
-    response = requests.request(method="GET", url=URL)
-    return response
-
-
-@pytest.SpecificWord("chuck_norris_api")
+@pytest.mark.usefixtures("chuck_norris_api")
 class TestSpecificWord:
     def test_specific_word(self):
-        print(chuck_norris_api.text)
+        print(self.response.text)
 
-    def test_status_code(self, chuck_norris_api):
-        URL = "https://api.chucknorris.io/jokes/search?query=Horses"
-        response = requests.request(method="GET", url=URL)
-        assert response.status_code == 200
+    def test_status_code(self):
+        assert self.response.status_code == 200
 
-    def test_number_of_jokes(self):
-        URL = "https://api.chucknorris.io/jokes/search?query=Horses"
-        response = requests.request(method="GET", url=URL)
-        j_son = response.json()
-        assert "result" in j_son
-        nambers_of_jokes = len(j_son["result"])
-        assert nambers_of_jokes > 0
-        print(f"Numbers of jokes {nambers_of_jokes}")
+    def test_number_of_jokes_check_rusult(self):
+        assert "result" in self.j_son
+
+    def test_numbers_of_jokes(self):
+        numbers_of_jokes = len(self.j_son["result"])
+        assert numbers_of_jokes > 0
+        print(f"Numbers of jokes {numbers_of_jokes}")
 
     def test_specific_joke(self):
         specific_of_joke = "Chuck Norris is not hung like a horse. Horses are hung like Chuck Norris."
-        URL = "https://api.chucknorris.io/jokes/search?query=Horses"
-        response = requests.request(method="GET", url=URL)
-        j_son = response.json()
-        assert "result" in j_son
-        jokes = [joke['value'] for joke in j_son["result"]]
+        assert "result" in self.j_son
+        jokes = [joke['value'] for joke in self.j_son["result"]]
         assert specific_of_joke in jokes
