e5fa3c4072c22068bf1bd985e98b81421ff3a307
solomon <solomon.1987.2020@gmail.com>
2024-02-08 22:25:20 +0200

    HW 20

--- HW_Kramarev_demoqa_com/conftest.py
+++ HW_Kramarev_demoqa_com/conftest.py
@@ -7,3 +7,18 @@ def chrome():
     driver = webdriver.Chrome()
     yield driver
     driver.quit()
+
+@pytest.fixture(scope="class")
+def firefox(request):
+    driver = webdriver.Firefox()
+    request.cls.driver = driver
+    driver.implicitly_wait(5)
+    yield driver
+    driver.quit()
+
+@pytest.fixture(scope="class")
+def chrome_class(request):
+    driver = webdriver.Chrome()
+    request.cls.driver = driver
+    yield driver
+    driver.quit()
