0050dd1febd5382f74e4898c6198b89eb7ac710c
solomon <solomon.1987.2020@gmail.com>
2024-02-25 23:39:54 +0200

    HW 23

---
+++ HW_Kramarev_sinoptik_ua/lokators.py
@@ -0,0 +1,16 @@
+
+url = https://ua.sinoptik.ua/
+
+#By.XPATH
+Search_area = "//input[contains(@id,'search_city')]"
+Language_switcher = "//a[contains(@title,'погода в Киеве')]"
+Days_switcher = "//div[@id='bd4']"
+Number_days_switch = "//a[@href='//ua.sinoptik.ua/погода-київ/10-днів']"
+Weather_map = "//a[contains(@class,'otherCity')]"
+
+#By.CSS
+Search_area_1 = "input#search_city"
+Language_switcher_2 = "div#sLang"
+Days_switcher_3 = "div#bd4"
+Number_days_switch_4 = "a.menu-item"
+Weather_map_5 = "a.otherCity"
