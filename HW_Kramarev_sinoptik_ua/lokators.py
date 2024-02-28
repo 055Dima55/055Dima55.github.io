4e5456ac8d7abfbb9daba7ab5468cb871f2b6fca
solomon <solomon.1987.2020@gmail.com>
2024-02-28 21:56:41 +0200

    HW 23

--- HW_Kramarev_sinoptik_ua/lokators.py
+++ HW_Kramarev_sinoptik_ua/lokators.py
@@ -4,9 +4,9 @@ url = "https://ua.sinoptik.ua/"
 Search_area = "//input[contains(@id,'search_city')]"
 # Search_area: Шукає поле введення для пошуку міста.
 Language_switcher = "//a[contains(@title,'погода в Киеве')]"
-# Language_switcher: Шукає перемикач мови для погоди в Києві
+# Language_switcher: Шукає перемикач мови для погоди в Києві.
 Days_switcher = "//div[@id='bd4']"
-# Days_switcher: Шукає блок з перемикачем днів
+# Days_switcher: Шукає блок з перемикачем днів.
 Number_days_switch = "//a[@href='//ua.sinoptik.ua/погода-київ/10-днів']"
 # Number_days_switch: Шукає посилання на прогноз на 10 днів.
 Weather_map = "//a[contains(@class,'otherCity')]"
