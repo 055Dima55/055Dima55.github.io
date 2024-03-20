ec65b4580911a3c67fa5fc247753e962b600391e
solomon <solomon.1987.2020@gmail.com>
2024-03-20 10:40:18 +0200

    HW 23

--- HW_23_Kramarev/HW_random.py
+++ HW_23_Kramarev/HW_random.py
@@ -1,10 +1,37 @@
 import random
 
+print("Виберіть набори символів, які потрібно включити:")
+print("1. Кирилиця")
+print("2. Латинські букви")
+print("3. Цифри")
+print("4. Великі букви")
+print("5. Маленькі букви")
 
-password_length = int(input('Enter a number of at least 5: '))
-list = (1, 2, 3, 4, 5, 6, 7, 8, 'A', 'a', 'B', 'b', 'C', 'c', 'Ц', 'Г', 'х', 'й', 'Т',)
+options = {
+    '1': 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ',
+    '2': 'АаБбВвГгДдЕеЄєЖжЗзИиІіЇїЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщьЮюЯя',
+    '3': '0123456789'
+}
 
-our_pass = []
-for i in range(0, password_length):
-    our_pass.append(random.choice(list))
-print(our_pass)
+chosen_sets = input("Введіть номери наборів символів (наприклад, '123'): ")
+password_length = int(input("Введіть довжину пароля: "))
+
+alphabet = ''
+
+for i in chosen_sets:
+    if i in options:
+        alphabet += options[i]
+
+if '4' in chosen_sets:
+    alphabet = alphabet.upper()
+
+if '5' in chosen_sets:
+    alphabet = alphabet.lower()
+
+if alphabet:
+    password = ""
+    for _ in range(password_length):
+        password += random.choice(alphabet)
+    print("Ваш згенерований пароль:", password)
+else:
+    print("Ви не вибрали жодного набору символів")
